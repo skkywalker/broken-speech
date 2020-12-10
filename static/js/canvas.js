c681728ab5d813231a237846182ea5d8ea62f5c2
Anne Sanflorian <47899278+annesanflorian@users.noreply.github.com>
2020-12-10 19:25:21 -0300

    A few improvements (#2)
    * add rules; 3 side buttons(pencil/eraser/undo) and modify cursors
    
    * delete trash info inside rooms.json

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -8,11 +8,17 @@ window.onload = function () {
     var mouseX = 0;
     var mouseY = 0;
     context.strokeStyle = 'black'; // initial brush color
-    context.lineWidth = 1; // initial brush width
+    context.lineWidth = 3; // initial brush width
+    context.lineJoin = 'round';
     var isDrawing = false;
+    
+    // Variables used on the undo button
+    var colora = 'black'; 
+    var linewidtha = 3; 
+    let points = [];
+    let pathsry = [];
 
-    context.strokeStyle = 'black';
-    context.lineWidth = 1;
+    document.body.style.cursor="url('/static/img/pencil.png'), auto";
 
     // Mouse Down Event
     canvas.addEventListener('mousedown', function(event) {
@@ -22,6 +28,12 @@ window.onload = function () {
         // Start Drawing
         context.beginPath();
         context.moveTo(mouseX, mouseY);
+
+        points = [];
+
+        // Save the coords, color and linewidth to use on the undo button
+        points.push({x:mouseX,y:mouseY,color:colora,linewidth:linewidtha});
+          
     });
 
     // Mouse Move Event
@@ -29,8 +41,11 @@ window.onload = function () {
         setMouseCoordinates(event);
 
         if(isDrawing){
-        context.lineTo(mouseX, mouseY);
-        context.stroke();
+            context.lineTo(mouseX, mouseY);
+            context.stroke();
+
+            // Save the coords, color and linewidth to use on the undo button
+            points.push({x:mouseX,y:mouseY,color:colora,linewidth:linewidtha});
         }
     });
 
@@ -38,7 +53,9 @@ window.onload = function () {
     canvas.addEventListener('mouseup', function(event) {
         setMouseCoordinates(event);
         isDrawing = false;
-    });
+        pathsry.push(points);
+        
+    }, false);
 
     // Handle Mouse Coordinates
     function setMouseCoordinates(event) {
@@ -46,11 +63,15 @@ window.onload = function () {
         mouseY = event.clientY - boundings.top;
     }
 
+/* --- Bottom Buttons --- */
+
     // Handle Clear Button
     var clearButton = document.getElementById('clear');
 
     clearButton.addEventListener('click', function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
+        points = [];
+        pathsry = [];
     });
 
     // Handle Save Button
@@ -68,4 +89,61 @@ window.onload = function () {
             }
           });
     });
+
+/* --- Side Buttons --- */
+
+    // Handle Pencil
+    var pencil = document.getElementById('pencil');
+    pencil.addEventListener('click', function() {
+      context.strokeStyle = 'black';
+      context.lineWidth = 3; 
+      document.body.style.cursor="url('/static/img/pencil.png'), auto";
+      
+      // Save the color/linewidth to use on the undo button
+      colora = context.strokeStyle;
+      linewidtha = context.lineWidth;      
+    });
+
+    // Handle Eraser
+    var eraser = document.getElementById('eraser');
+    eraser.addEventListener('click', function() {
+      context.strokeStyle = '#ffffff';
+      context.lineWidth = 15; 
+      document.body.style.cursor="url('/static/img/eraser1.png'), crosshair"; 
+
+      // Save the color/linewidth to use on the undo button
+      colora = context.strokeStyle;
+      linewidtha = context.lineWidth;      
+    });
+
+    // Handle undo
+    undo.addEventListener("click",Undo);
+
+    function drawPaths(){
+      // delete everything
+      context.clearRect(0,0,canvas.width,canvas.height);
+      // draw all the paths in the paths array
+
+      pathsry.forEach(path=>{
+      for(let i = 0; i < path.length-1; i++){
+        context.beginPath();
+        context.moveTo(path[i].x,path[i].y);  
+        context.lineTo(path[i+1].x,path[i+1].y);
+        context.strokeStyle = path[i].color;
+        context.lineWidth = path[i].linewidth;
+        context.stroke();
+      }
+      })
+    }  
+    
+    function Undo(){
+      // remove the last path from the paths array
+      pathsry.splice(-1,1);
+      // draw all the paths in the paths array
+      drawPaths();
+
+      // continue drawing with the same tool (pencil/eraser)
+      if(colora == '#ffffff'){context.strokeStyle = '#ffffff';context.lineWidth = 15;}
+      else {context.strokeStyle = '#000000';context.lineWidth = 3;}
+    }
 };
